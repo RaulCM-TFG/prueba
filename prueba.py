a1e7b36df0cacd4a937cc214d2278b969376f700
RaulCM <raulcanomontero@hotmail.com>
2020-11-03 17:35:20 +0100

    Fix Pylint Errors

--- prueba.py
+++ prueba.py
@@ -1,20 +1,21 @@
 from fork import USERNAME
 from fork import PASSWORD
-import os, requests
-
-x=5
-y  = 6
+import os
 import requests
-z = x+y;text = "Z=11"
-if z==11:
+
+x = 5
+y = 6
+z = x+y
+text = "Z=11"
+if z == 11:
     print(z)
     print(x)
 print(os.chdir("/"))
 print(text)
 r = requests.get('?access_token=')
-tokens = text.split("=",(1))
+tokens = text.split("=", (1))
 print(tokens)
-numbers = [1, 2,3 ]
+numbers = [1, 2, 3]
 print(numbers)
 print(USERNAME)
 print(PASSWORD)
