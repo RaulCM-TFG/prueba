0a3d628b5ea86bf6bf063e3c5011700f7d07149d
RaulCM <raulcanomontero@hotmail.com>
2021-06-20 10:40:14 +0200

    Test

---
+++ d2/d21/prueba.py
@@ -0,0 +1,20 @@
+from fork import USERNAME
+from fork import PASSWORD
+import os, requests
+
+x=5
+y  = 6
+import requests
+z = x+y;text = "Z=11"
+if z==11:
+    print(z)
+    print(x)
+print(os.chdir("/"))
+print(text)
+r = requests.get('?access_token=')
+tokens = text.split("=",(1))
+print(tokens)
+numbers = [1, 2,3 ]
+print(numbers)
+print(USERNAME)
+print(PASSWORD)
